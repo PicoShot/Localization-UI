0394f7ce099cac13d69e65ad89791f493080cba8
Pico <picoshot241@gmail.com>
2026-03-10 06:12:04 +0300

    style: remove bloat in detail panel

--- src/components/views/editor/KeyDetailsPanel.tsx
+++ src/components/views/editor/KeyDetailsPanel.tsx
@@ -1,13 +1,4 @@
-import {
-  Flex,
-  Box,
-  Text,
-  Heading,
-  ScrollArea,
-  TextArea,
-  Button,
-} from "@radix-ui/themes";
-import { Edit2, Languages, Copy, Code2, Eraser, Trash2 } from "lucide-react";
+import { Flex, Box, Text, Heading, ScrollArea } from "@radix-ui/themes";
 import { LocaleData } from "../../../lib/bloc";
 import { UnifiedKey } from "./types";
 import { StringKeyEditor } from "./StringKeyEditor";
@@ -23,12 +14,13 @@ export function KeyDetailsPanel({
   locales,
   selectedKey,
 }: KeyDetailsPanelProps) {
-  const deleteSelectedKey = useEditorStore((s) => s.deleteSelectedKey);
   const setStringValue = useEditorStore((s) => s.setStringValue);
   const setArrayElement = useEditorStore((s) => s.setArrayElement);
   const removeArrayElement = useEditorStore((s) => s.removeArrayElement);
   const addArrayElement = useEditorStore((s) => s.addArrayElement);
-  const clearEmptyArrayElements = useEditorStore((s) => s.clearEmptyArrayElements);
+  const clearEmptyArrayElements = useEditorStore(
+    (s) => s.clearEmptyArrayElements,
+  );
 
   if (!selectedKey) {
     return (
@@ -56,54 +48,13 @@ export function KeyDetailsPanel({
           </Heading>
         </Box>
 
-        <Box>
-          <Text
-            as="label"
-            size="2"
-            weight="bold"
-            color="gray"
-            mb="1"
-            style={{ display: "block" }}
-          >
-            Translation Hint
-          </Text>
-          <TextArea placeholder="Context for translators/DeepL..." size="2" />
-        </Box>
-
-        <Flex
-          gap="2"
-          wrap="wrap"
-          align="center"
-          style={{
-            borderBottom: "1px solid var(--gray-a4)",
-            paddingBottom: "16px",
-          }}
-        >
-          <Button variant="soft" size="1" color="gray">
-            <Edit2 size={14} /> Rename
-          </Button>
-          <Button variant="soft" size="1" color="blue">
-            <Languages size={14} /> Translate
-          </Button>
-          <Button variant="soft" size="1" color="gray">
-            <Copy size={14} /> Copy
-          </Button>
-          <Button variant="soft" size="1" color="amber">
-            <Code2 size={14} /> JSON
-          </Button>
-          <Button variant="soft" size="1" color="gray">
-            <Eraser size={14} /> Clear
-          </Button>
-          <Button variant="soft" size="1" color="red" onClick={deleteSelectedKey}>
-            <Trash2 size={14} /> Delete
-          </Button>
-        </Flex>
-
         {selectedKey.type === "string" && (
           <StringKeyEditor
             locales={locales}
             selectedKey={selectedKey}
-            onChange={(langCode, newValue) => setStringValue(keyName, langCode, newValue)}
+            onChange={(langCode, newValue) =>
+              setStringValue(keyName, langCode, newValue)
+            }
           />
         )}
 
@@ -111,7 +62,9 @@ export function KeyDetailsPanel({
           <ArrayKeyEditor
             locales={locales}
             selectedKey={selectedKey}
-            onElementChange={(langCode, index, newValue) => setArrayElement(keyName, langCode, index, newValue)}
+            onElementChange={(langCode, index, newValue) =>
+              setArrayElement(keyName, langCode, index, newValue)
+            }
             onRemoveElement={(index) => removeArrayElement(keyName, index)}
             onAddElement={() => addArrayElement(keyName)}
             onClearEmpty={() => clearEmptyArrayElements(keyName)}
