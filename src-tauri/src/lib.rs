8a1d5ef5d23a9010363ce749cf6c9083a8714a36
Pico <picoshot241@gmail.com>
2026-03-12 03:51:51 +0300

    feat: add single instance & updater plugins

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,25 +1,20 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let mut builder = tauri::Builder::default();
 
-    // #[cfg(desktop)]
-    // {
-    //     builder = builder.plugin(tauri_plugin_single_instance::init(|app, _, _| {
-    //         use tauri::Manager;
-    //
-    //         let _ = app
-    //             .get_webview_window("main")
-    //             .expect("no main window")
-    //             .set_focus();
-    //     }));
-    // }
+    #[cfg(all(desktop, not(debug_assertions)))]
+    {
+        builder = builder.plugin(tauri_plugin_single_instance::init(|app, _, _| {
+            use tauri::Manager;
 
+            let _ = app
+                .get_webview_window("main")
+                .expect("no main window")
+                .set_focus();
+        }));
+    }
+
+    builder = builder.plugin(tauri_plugin_updater::Builder::new().build());
     builder = builder.plugin(tauri_plugin_os::init());
     builder = builder.plugin(tauri_plugin_keyring::init());
     builder = builder.plugin(tauri_plugin_websocket::init());
@@ -28,7 +23,6 @@ pub fn run() {
     builder = builder.plugin(tauri_plugin_fs::init());
     builder = builder.plugin(tauri_plugin_persisted_scope::init());
     builder = builder.plugin(tauri_plugin_dialog::init());
-    builder = builder.invoke_handler(tauri::generate_handler![greet]);
 
     builder
         .run(tauri::generate_context!())
